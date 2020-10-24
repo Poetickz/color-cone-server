508eacc3455c1a47f7863436555ddbde94f54764
Alan Rocha <alan.rocha@udem.edu>
2020-10-24 15:40:52 -0500

    Fixed app port

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ import time
 
 fake_request = json.loads(open('image2.json', 'r').read())
 
+url = 'https://color-cone-server.herokuapp.com/ '
+
 string = fake_request['img']
 jpg_original = base64.b64decode(string)
 jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
@@ -15,7 +17,7 @@ img = cv2.imdecode(jpg_as_np, flags=1)
 cv2.imshow( "Display window", img )
 cv2.waitKey(0)
 
-response = requests.post('http://localhost:5000/', json=fake_request)
+response = requests.post(f'{url}', json=fake_request)
 
 
 print("Status code: ", response.status_code)
@@ -26,7 +28,7 @@ job_id = response.json()['job_id']
 
 time.sleep(3)
 
-response = requests.get(f'http://localhost:5000/results/{job_id}')
+response = requests.get(f'{url}/results/{job_id}')
 print(response.json())
 
 
